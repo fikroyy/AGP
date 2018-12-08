de3a2197a926f5b1b05bc4af4ab2992bba0ac326
monhack <fikri120698@gmail.com>
2018-12-08 18:20:24 +0700

    ada sedikit perubahan dibagian web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -8,9 +19,10 @@ Route::get('/menu', function () {
     return view('/Agp/menu');
 });
 
+Auth::routes();
+
 Route::get('/booking', function () {
 	return view('/Agp/booking');
 });
-Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
